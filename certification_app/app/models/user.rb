f46b9e7cccfcbe5e7f893908219b683fe5b7443c
Ayush Bansal <ayushb268@gmail.com>
2019-04-12 00:07:05 +0530

    Add basic user validations

--- certification_app/app/models/user.rb
+++ certification_app/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   self.primary_key = 'public_addr'
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :public_addr, presence: true, length: {is: 40}, uniqueness: true
+
   has_many :courses
   has_many :user_course_mappings
   has_many :courses_taken, class_name: 'Course', through: :user_course_mappings, :source => :course
